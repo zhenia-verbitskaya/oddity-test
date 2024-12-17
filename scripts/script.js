b0160209bb0fe55f88a1582b03bae5fbed476e92
Zhenia Verbitskaya <jvzns17@gmail.com>
2024-12-17 17:01:02 +0200

    initial commit

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+const luckBtn = document.querySelector(".luck-btn");
+const changeLogo = document.querySelector(".logo-img");
+luckBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  changeLogo.src = "../images/oddity-logo.jpg";
+});
