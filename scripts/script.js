f674d411749a431a0d47047fd926b236aadbb580
Zhenia Verbitskaya <jvzns17@gmail.com>
2024-12-17 17:09:52 +0200

    fix image path

--- scripts/script.js
+++ scripts/script.js
@@ -2,5 +2,5 @@ const luckBtn = document.querySelector(".luck-btn");
 const changeLogo = document.querySelector(".logo-img");
 luckBtn.addEventListener("click", function (event) {
   event.preventDefault();
-  changeLogo.src = "../images/oddity-logo.jpg";
+  changeLogo.src = "./images/oddity-logo.jpg";
 });
